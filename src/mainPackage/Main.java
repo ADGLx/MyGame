12a2493b0efc803e9c68cde65f331bc21d8b1644
Alvaro Gonzalez <alvarodavidglx@hotmail.com>
2020-11-11 23:41:36 -0500

    Implemented the first few things to display a window

--- src/mainPackage/Main.java
+++ src/mainPackage/Main.java
@@ -1,40 +1,63 @@
 package mainPackage;
+import static org.lwjgl.glfw.GLFW.*;
+
+import org.lwjgl.glfw.GLFW;
+
+import engine.io.*;
 
 public class Main implements Runnable{
 
 public Thread game;
+public static CustomWindow window;
+public final static int width = 1280, height = 720;
 	
 	public void start() {
 		game = new Thread(this, "game");
 		game.start();
 	}
 	
-	public static void init() {
-		System.out.println("Initializing Game!");
+	public void init() { //used to init
+		//System.out.println("Initializing Game!");
+		window = new CustomWindow(width,height,"Java Game v0.01");
+		window.create();
 	}
 	
 	public void run() {
 		init();
-		while (true) {
+		while (!window.shouldClose()) {
 			update();
 			render();
+			if (Input.isKeyDown(GLFW.GLFW_KEY_ESCAPE)) return; //exit game if escape(Move to another script later 
 		} 
 	}
 	
 	private void update() {
-		System.out.println("Updating Game!");
+		//System.out.println("Updating Game!");
+		window.update();
+		
+		//input stuff
+		if (Input.isMButtonDown(GLFW.GLFW_MOUSE_BUTTON_LEFT)) System.out.println("X: " + Input.getMouseX() + ", Y: " + Input.getMouseY()); //Debug delete later 
+
 	}
 	
 	private void render() {
-		System.out.println("Rendering Game!");
+		//System.out.println("Rendering Game!");
+		window.swapBuffers();
 	}
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(org.lwjgl.Version.getVersion() +"TEST");
+		if (!glfwInit())
+		{
+			throw new IllegalStateException("Failed to initialize GLFW!");
+		} else {
+			System.out.println(org.lwjgl.Version.getVersion());
+		}
 		
-		new Main().start();
+
+		
+		new Main().start(); //this starts the update 
 	}
 
 }
